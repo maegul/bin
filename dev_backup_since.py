a72a0d1a1f857496ea4ad14baff73f6109de0959
Errol Lloyd <errolkjlloyd@gmail.com>
2021-10-13 21:17:05 +1100

    cleaner dev backup since

--- dev_backup_since.py
+++ dev_backup_since.py
@@ -8,7 +8,11 @@ last_success = success_times[-1]
 
 last_success_time = dt.datetime.fromisoformat(last_success)
 
-print(f'{dt.datetime.now().astimezone() - last_success_time.astimezone()}')
+time_since = dt.datetime.now().astimezone() - last_success_time.astimezone()
+
+time_since_days = time_since.total_seconds() / (3600 * 24)
+
+print(f'{time_since_days:.2f}')
 
 
 
